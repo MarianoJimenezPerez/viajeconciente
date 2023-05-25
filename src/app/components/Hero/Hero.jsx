9503151a99bddc58783aab315cd9f05b4f756f42
MarianoJimenezPerez <82915592+MarianoJimenezPerez@users.noreply.github.com>
2023-05-25 13:04:34 -0300

    Se agrega componente FeatureBlogs y se estila dropdown de botón de hamburguesa de Header

---
+++ src/app/components/Hero/Hero.jsx
@@ -0,0 +1,36 @@
+export default function Hero() {
+  return (
+    <div className="container">
+      <div className="hero">
+        <div className="left">
+          <div className="blog__card blog__card-big">
+            <img src="https://avada.website/magazine/wp-content/uploads/sites/166/2023/02/imp-things-about-smartphones.jpg" />
+            <div className="blog__card__content">
+              <span>Technology</span>
+              <h3>Important Trends You Need To Know About Social Media</h3>
+              <p>By Atif | Last Updated: April 7, 2023</p>
+            </div>
+          </div>
+        </div>
+        <div className="right">
+          <div className="blog__card">
+            <img src="https://avada.website/magazine/wp-content/uploads/sites/166/2023/02/imp-things-about-smartphones.jpg" />
+            <div className="blog__card__content">
+              <span>Technology</span>
+              <h3>Important Trends You Need To Know About Social Media</h3>
+              <p>By Atif | Last Updated: April 7, 2023</p>
+            </div>
+          </div>
+          <div className="blog__card">
+            <img src="https://avada.website/magazine/wp-content/uploads/sites/166/2023/02/imp-things-about-smartphones.jpg" />
+            <div className="blog__card__content">
+              <span>Technology</span>
+              <h3>Important Trends You Need To Know About Social Media</h3>
+              <p>By Atif | Last Updated: April 7, 2023</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,36 +1,10 @@
+import FeatureBlogs from "./components/FeatureBlogs/FeatureBlogs";
+import Hero from "./components/Hero/Hero";
 export default function HomePage() {
   return (
-    <main className="container">
-      <div className="hero">
-        <div className="left">
-          <div className="blog__card blog__card-big">
-            <img src="https://avada.website/magazine/wp-content/uploads/sites/166/2023/02/imp-things-about-smartphones.jpg" />
-            <div className="blog__card__content">
-              <span>Technology</span>
-              <h3>Important Trends You Need To Know About Social Media</h3>
-              <p>By Atif | Last Updated: April 7, 2023</p>
-            </div>
-          </div>
-        </div>
-        <div className="right">
-          <div className="blog__card">
-            <img src="https://avada.website/magazine/wp-content/uploads/sites/166/2023/02/imp-things-about-smartphones.jpg" />
-            <div className="blog__card__content">
-              <span>Technology</span>
-              <h3>Important Trends You Need To Know About Social Media</h3>
-              <p>By Atif | Last Updated: April 7, 2023</p>
-            </div>
-          </div>
-          <div className="blog__card">
-            <img src="https://avada.website/magazine/wp-content/uploads/sites/166/2023/02/imp-things-about-smartphones.jpg" />
-            <div className="blog__card__content">
-              <span>Technology</span>
-              <h3>Important Trends You Need To Know About Social Media</h3>
-              <p>By Atif | Last Updated: April 7, 2023</p>
-            </div>
-          </div>
-        </div>
-      </div>
+    <main>
+      <Hero />
+      <FeatureBlogs />
     </main>
   );
 }
